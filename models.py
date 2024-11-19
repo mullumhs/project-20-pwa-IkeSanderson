36fed48ff5545cb5b6b9a6331e2efb63835a13cf
IkeSanderson <140124177+IkeSanderson@users.noreply.github.com>
2024-11-20 10:39:35 +1100

    Created the data base model

--- models.py
+++ models.py
@@ -4,3 +4,13 @@ db = SQLAlchemy()
 
 # Define your database model here
 # Example: class Item(db.Model):
+class Fish(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    common_name = db.Column(db.String(100), nullable = False)
+    scientific_name = db.Column(db.String(100))
+    species = db.Column(db.String(100), )
+    water_type = db.Column(db.String(100))
+    avg_length = db.Column(db.Integer)
+    shape = db.Column(db.String(100))
+    avg_life_span = db.Column(db.Integer)
+    
