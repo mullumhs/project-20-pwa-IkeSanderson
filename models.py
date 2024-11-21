7bc4c1b48efb9d896cb499f230000a9faf219b7b
IkeSanderson <140124177+IkeSanderson@users.noreply.github.com>
2024-11-22 10:40:37 +1100

    Expanded index.html and updated base.html

--- models.py
+++ models.py
@@ -8,10 +8,10 @@ class Fish(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     common_name = db.Column(db.String(100), nullable = False)
     scientific_name = db.Column(db.String(100))
-    species = db.Column(db.String(100), )
+    species = db.Column(db.String(100))
     water_type = db.Column(db.String(100))
     avg_length = db.Column(db.Integer)
+    avg_lifespan = db.Column(db.Integer)
     shape = db.Column(db.String(100))
-    avg_life_span = db.Column(db.Integer)
-    #legs = db.Column(db.Bool)
+    legs = db.Column(db.Boolean, default = False)
     
