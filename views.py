61273ef430a49e09a96e1cbf2a8409f8062bb716
IkeSanderson <140124177+IkeSanderson@users.noreply.github.com>
2024-11-25 09:50:11 +1100

    Began add.html and started adding functionality through the add route

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, redirect, url_for, flash
 from models import db # Also import your database model here
+from views import Fish
 
 # Define your routes inside the 'init_routes' function
 # Feel free to rename the routes and functions as you see fit
@@ -16,11 +17,25 @@ def init_routes(app):
 
 
 
-    @app.route('/add', methods=['POST'])
+    @app.route('/add', methods=['GET','POST'])
     def create_item():
-        # This route should handle adding a new item to the database.
-        return render_template('index.html', message='Item added successfully')
+        if request.method == 'POST':
+            new_fish = Fish(
+                common_name = request.form['common_name'],
+                scientific_name = request.form['scientific_name'],
+                species = request.form['species'],
+                shape = request.form['shape'],
+                avg_length = int(request.form['avg_length']),
+                avg_lifespan = int(request.form['avg_lifespan'])
+                water_type = request.form['water_type'],
+                has_legs = request.form['has_legs'],
+            )
 
+            db.session.add(new_fish)
+            db.session.commit()
+            return render_template('index.html', message='Fish added successfully')
+        
+        return render_template('add.html')
 
 
     @app.route('/update', methods=['POST'])
